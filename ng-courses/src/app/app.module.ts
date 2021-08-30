d5493632bc2a330d3405be47e89a14ac972d5164
AlexeyEmshanov <alexey.emshanov@gmail.com>
2021-08-30 15:46:41 +0400

    feat: add main components

--- ng-courses/src/app/app.module.ts
+++ ng-courses/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CoursesPageComponent } from './components/courses-page/courses-page.component';
+import { CourseComponent } from './components/course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    FooterComponent,
+    CoursesPageComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
