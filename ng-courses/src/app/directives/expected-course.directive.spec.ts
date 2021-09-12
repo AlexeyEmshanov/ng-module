27ce536075dd2b6b5596440f3db05932b48d026e
AlexeyEmshanov <alexey.emshanov@gmail.com>
2021-09-12 16:43:26 +0400

    feat: trying t implement unit test for expected-course directive

--- ng-courses/src/app/directives/expected-course.directive.spec.ts
+++ ng-courses/src/app/directives/expected-course.directive.spec.ts
@@ -1,9 +1,14 @@
 import { ElementRef } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CourseItemComponent } from '../components/course-item/course-item.component';
 import { ExpectedCourseDirective } from './expected-course.directive';
 
 describe('ExpectedCourseDirective', () => {
-  // it('should create an instance', () => {
-  //   const directive = new ExpectedCourseDirective();
-  //   expect(directive).toBeTruthy();
-  // });
+  let component: CourseItemComponent;
+
+  it('should create an instance', () => {
+    const directive = new ExpectedCourseDirective(new ElementRef(component));
+    console.log('testing', directive);
+    expect(directive).toBeTruthy();
+  });
 });
