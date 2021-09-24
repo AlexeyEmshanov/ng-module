a99af80701d548c59e8ee89ce2b9d2dbf57da0be
alexeyemshanov <alexey.emshanov@gmail.com>
2021-09-24 11:04:22 +0400

    feat: finish with unit tests at durationCoursePipe

--- ng-courses/src/app/pipes/duration-course.pipe.spec.ts
+++ ng-courses/src/app/pipes/duration-course.pipe.spec.ts
@@ -1,24 +1,28 @@
 import { DurationCoursePipe } from './duration-course.pipe';
 
 describe('DurationCoursePipe', () => {
+  const pipe = new DurationCoursePipe();
+
+
   it('create an instance', () => {
-    const pipe = new DurationCoursePipe();
     expect(pipe).toBeTruthy();
   });
 
   it('transform 150 to 2h 30min', () => {
-    const pipe = new DurationCoursePipe();
     expect(pipe.transform(150)).toBe('2h 30min');
   });
 
   it('transform 50 to 50min', () => {
-    const pipe = new DurationCoursePipe();
     expect(pipe.transform(50)).toBe('50min');
   });
 
   it('transform 125 to 2h 05min', () => {
-    const pipe = new DurationCoursePipe();
     expect(pipe.transform(125)).toBe('2h 05min');
+  });
+
+  it('if duration is undefined should return warning "invalid course duration format"', () => {
+    const warning = 'invalid course duration format'
+    expect(pipe.transform(undefined)).toBe(warning);
   });
 
 });
