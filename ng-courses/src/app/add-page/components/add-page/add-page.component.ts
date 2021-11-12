5043d280cec8a5473e7e67ad5ff15b836d43925e
AlexeyEmshanov <alexey.emshanov@gmail.com>
2021-11-12 22:29:56 +0400

    feat: finish data form validation

--- ng-courses/src/app/add-page/components/add-page/add-page.component.ts
+++ ng-courses/src/app/add-page/components/add-page/add-page.component.ts
@@ -1,48 +1,47 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { update } from 'lodash';
 import { Course } from 'src/app/model/course';
 import { ICourse } from 'src/app/model/interfaces/icourse';
 import { CoursesService } from 'src/app/services/courses.service';
 import { NewCourse } from '../../model/newCourse';
+import * as moment from 'moment'
 
 @Component({
   selector: 'app-add-page',
   templateUrl: './add-page.component.html',
   styleUrls: ['./add-page.component.scss'],
 })
-export class AddPageComponent {
+export class AddPageComponent implements OnInit {
   addCourseForm: FormGroup = new FormGroup(
     {
       titleCtrl: new FormControl('', [ Validators.required, Validators.maxLength(10) ],),
       descriptionCtrl: new FormControl('', [ Validators.required, Validators.maxLength(20) ]),
-      dateCtrl: new FormControl(new Date(), [Validators.required]),
+      dateCtrl: new FormControl(new Date(), [] ),
     },
-    { updateOn: 'blur' }
+    { updateOn: 'change' }
   )
-
-
-  // public newCourse = new Course(this.generateID(), '', '', false, new Date(), [], 0);
 
   constructor(private coursesService: CoursesService, private router: Router, private formBuilder: FormBuilder) { }
 
-  // ngOnInit(): void {
-  // }
+  ngOnInit(): void {
+    this.addCourseForm.controls.dateCtrl.valueChanges.subscribe(data => {console.log('STATUS: ', this.addCourseForm.pristine)})
+  }
 
   public generateID() {
     return Math.floor(Math.random() * 100) + 1
   }
 
   public generateCourse () {
-    console.log('test 2', this.addCourseForm.get('date')?.value)
+    console.log('test 2', this.addCourseForm.get('dateCtrl')?.value)
     const newCourse = new Course(
       this.generateID(),
-      this.addCourseForm.get('title')?.value,
-      this.addCourseForm.get('description')?.value,
+      this.addCourseForm.get('titleCtrl')?.value,
+      this.addCourseForm.get('descriptionCtrl')?.value,
       false,
-      this.addCourseForm.get('date')?.value,
+      this.addCourseForm.get('dateCtrl')?.value,
       [],
       50
       )
@@ -51,9 +50,14 @@ export class AddPageComponent {
   }
 
   public onSubmit() {
-    console.log('Submitting', this.addCourseForm.get('title')?.value, this.addCourseForm.get('description')?.value)
-    console.log(this.addCourseForm.status, this.addCourseForm.dirty, this.addCourseForm.touched);
+    console.log('Submitting', this.addCourseForm.get('titleCtrl')?.value, this.addCourseForm.get('descriptionCtrl')?.value)
+    console.log(this.addCourseForm.status, this.addCourseForm.dirty, this.addCourseForm.touched, this.addCourseForm.pristine);
     this.coursesService.createCourse(this.generateCourse()).subscribe();
     this.router.navigate(['courses']);
   }
+
+  public fromAbsToControl(absCtrl: AbstractControl): FormControl {
+    const ctrl = absCtrl as FormControl;
+    return ctrl as FormControl;
+}
 }
--- ng-courses/src/app/components/date-field/date-field.component.ts
+++ ng-courses/src/app/components/date-field/date-field.component.ts
@@ -1,23 +1,31 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { AbstractControl, ControlValueAccessor, FormControl, FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR, ValidationErrors, Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-date-field',
   templateUrl: './date-field.component.html',
   styleUrls: ['./date-field.component.scss'],
   providers: [
-    { provide: NG_VALUE_ACCESSOR, useExisting: DateFieldComponent, multi: true }
+    { provide: NG_VALUE_ACCESSOR, useExisting: DateFieldComponent, multi: true },
+    { provide: NG_VALIDATORS, useExisting: DateFieldComponent, multi: true }
   ]
 })
-export class DateFieldComponent implements OnInit, ControlValueAccessor {
-  dateValue: Date = new Date();
+export class DateFieldComponent implements OnInit, ControlValueAccessor, Validator {
+  dateValue?: Date;
+
+  @Input() childControl!: FormControl;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.dateValue = new Date();
+  }
 
   onChange: any = () => { }
   onTouch: any = () => { }
 
   writeValue(value: Date) {
     this.dateValue = value
-    console.log('Write value', typeof this.dateValue, this.dateValue);
   }
 
   registerOnChange(fn: any): void {
@@ -33,10 +41,13 @@ export class DateFieldComponent implements OnInit, ControlValueAccessor {
     this.onTouch();
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
-    this.dateValue = new Date();
-    console.log('On Init dateValue', this.dateValue)
+  public validate(control: AbstractControl): ValidationErrors | null {
+    return (control.value) ? null :  {
+      validateDate: {
+        dataInvalid: 'Date format is wrong'
+      }
+    }
   }
+
+
 }
